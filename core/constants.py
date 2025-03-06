60ee8f6890cf4f95667dbf22c284648df2dde2aa
Jorge Aldair Cortés López <aldacortes@MacBook-Pro-de-Jorge.local>
2025-03-06 15:47:12 -0600

    First commit with structure

---
+++ core/constants.py
@@ -0,0 +1,14 @@
+# This file will contain parametrizable constants for the project
+# At the beginning of the project, we can allow to set these constants at 
+# module level, later, we can move these to a configuration file, or to specific classes
+
+from enum import Enum
+
+
+class PROBABILITY_DISTRIBUTIONS(Enum):
+    """
+    As long as the project envolve working 
+    with some of the common probability distributions,
+    we can define them here
+    """
+    pass
